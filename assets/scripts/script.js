28c5ab0a7d96738530657a59fc92a57bc77f94bd
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-21 15:40:07 +0100

    completed css styling

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+/* Generare una griglia di gioco quadrata, simile a quella dello screenshot, in cui ogni cella contiene un numero tra 1 e 100.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+Bonus
+Permettere all'utente di indicare una difficoltà in base alla quale viene generato un numero variabile di celle:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49 */
